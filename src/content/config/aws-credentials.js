52101d6ed07eee34897e81c92616dcfc35cc8e92
Miguel Nuno <miguel.nuno@oracle.com>
2019-11-14 19:57:22 -0600

    AWS config created

---
+++ src/content/config/aws-credentials.js
@@ -0,0 +1,8 @@
+export default {
+  user: "IoT-User",
+  accessKey: "AKIATR46AIQEZRA4QVFC",
+  secretKey: "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",
+  tableDB: "arn:aws:dynamodb:us-east-1:244607632393:table/IoT-DB",
+  tableDr: "arn:aws:dynamodb:us-east-1:244607632393:table/IoT-Dr",
+  tablePatient: "arn:aws:dynamodb:us-east-1:244607632393:table/IoT-Dr"
+}
--- src/content/pages/dashboard/DashBoard.js
+++ src/content/pages/dashboard/DashBoard.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ListData from './ListData'
+//import AWSInstance from '../../config/awsConfig' 
+import AWSConfig from '../../config/awsConfig';
 class DashBoard extends React.Component {
     render() {
+        console.log("HolaMundo")
+        let aws = new AWSConfig()
+        aws.onRead()
         return (
             <div id="DashBoard" className="tabcontent">
                 <h3>Home</h3>
